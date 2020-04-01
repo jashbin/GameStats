f984160441a80ba2efd6d57bdfa2d6c4d07585cb
Checkam <maxencedespres@gmail.com>
2020-04-01 16:20:32 +0200

    Ajout igdb component a l'application

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
+<!--
+@Author: Despres Maxence <checkam>
+@Date:   01-Apr-2020
+@Email:  maxence.despres.etu@univ-lemans.fr
+@Filename: App.vue
+@Last modified by:   checkam
+@Last modified time: 01-Apr-2020
+-->
+
+
+
 <template>
   <div id="app">
     <div class>
       <input v-model="research" placeholder="Search :" />
-      <button v-on:click="send()" type="button" name="button">Search</button>
+      <button v-on:click="send()" type="button" class="btn btn-primary" name="button">Search</button>
     </div>
 
     <!-- Twitch -->
@@ -12,18 +23,22 @@
     <b-container fluid class="p-0" style="height:500px; margin-top: 50px;">
       <Imgur :search="toSend" :heightImg="20" />
     </b-container>
+
+    <igdb :search="toSend" />
+
   </div>
 </template>
 
 <script>
 import Twitch from "./components/Twitch.vue";
 import Imgur from "./components/Imgur.vue";
-
+import Igdb from "./components/IGDB.vue";
 export default {
   name: "App",
   components: {
     Twitch,
-    Imgur
+    Imgur,
+    Igdb
   },
   data() {
     return {
@@ -46,6 +61,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
+  background: #202040;
 }
 </style>
