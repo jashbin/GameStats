4fd0f5de20f49a597c685235ef5fa759b984a506
jashbin <jasmin.galbrun@gmail.com>
2020-04-01 14:22:17 +0200

    Ajout twitch component à App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
+    <div class>
+      <input v-model="research" placeholder="Search :" />
+      <button v-on:click="send()" type="button" name="button">Search</button>
+    </div>
+
+    <!-- Twitch -->
+    <Twitch :search="toSend" />
+
     <!-- Imgur -->
     <b-container fluid class="p-0" style="height:500px; margin-top: 50px;">
-      <Imgur search="csgo" :heightImg="20" />
+      <Imgur :search="toSend" :heightImg="20" />
     </b-container>
   </div>
 </template>
 
 <script>
+import Twitch from "./components/Twitch.vue";
 import Imgur from "./components/Imgur.vue";
 
 export default {
   name: "App",
   components: {
+    Twitch,
     Imgur
+  },
+  data() {
+    return {
+      research: "",
+      toSend: ""
+    };
+  },
+  methods: {
+    send() {
+      this.toSend = this.research;
+    }
   }
 };
 </script>
