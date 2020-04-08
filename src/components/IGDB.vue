0ea98297214a3477edfe5785ed293acc4fd32fbc
Checkam <maxencedespres@gmail.com>
2020-04-08 12:19:38 +0200

    Modification IGDB

--- src/components/IGDB.vue
+++ src/components/IGDB.vue
@@ -7,33 +7,33 @@
 @Last modified time: 02-Apr-2020
 -->
 <template lang="html">
-  <b-col class="p-3 w-100" style="background-color: #602080; color: white; height: 430px">
-    <b-spinner v-show="!load" variant="danger" label="Spinning"></b-spinner>
-    <b-col class="" v-show="load" >
-        <b-row class="mb-3 flex-nowrap" style="height: 90px; font-size: 0.8m;">
-          <img :src="imageUrl" alt="" class="mr-3">
-          <div class="d-flex flex-column">
-            <b class="d-flex">{{name}}</b>
-            <span class="d-flex"> <b class="mr-2 text-nowrap"> Genre : </b>
-              <div v-for="(genre) in genres" :key="genre" class="mr-2 text-nowrap">
-                <!-- genre -->
-                {{genre}},
-              </div>
-            </span>
-            <span class="d-flex"> <b class="mr-2"> Theme : </b>
-              <div v-for="(t) in theme" :key="t" class="mr-2 text-nowrap">
-                <!-- theme -->
-                {{t}},
-              </div>
-            </span>
-          </div>
-        </b-row>
-        <b-row class="text-justify overflow-auto" style="height: 300px;">
-            <!-- description -->
-            {{description}}
-        </b-row>
+    <b-col class="p-2 w-100" style="background-color: #602080; color: white; height: 430px">
+      <b-spinner v-show="!load" variant="danger" label="Spinning"></b-spinner>
+      <b-col class="" v-show="load" >
+          <b-row class="mb-3 flex-nowrap" style="height: 90px; font-size: 0.8m;">
+            <img :src="imageUrl" alt="" class="mr-3">
+            <div class="d-flex flex-column">
+              <b class="d-flex">{{name}}</b>
+              <span class="d-flex"> <b class="mr-2 text-nowrap"> Genre : </b>
+                <div v-for="(genre) in genres" :key="genre" class="mr-2 text-nowrap">
+                  <!-- genre -->
+                  {{genre}},
+                </div>
+              </span>
+              <span class="d-flex"> <b class="mr-2"> Theme : </b>
+                <div v-for="(t) in theme" :key="t" class="mr-2 text-nowrap">
+                  <!-- theme -->
+                  {{t}},
+                </div>
+              </span>
+            </div>
+          </b-row>
+          <b-row class="text-justify overflow-auto" style="height: 300px;">
+              <!-- description -->
+              {{description}}
+          </b-row>
+      </b-col>
     </b-col>
-  </b-col>
 </template>
 
 <script>
@@ -105,19 +105,21 @@ export default {
       let self = this;
       self.load = false;
       await self.getGame(self.search, function(data){
-        self.name = data[0].name;
-        self.description = data[0].summary;
-        self.id = data[0].id;
-        self.getCoverGame(data[0].cover,function(data){
+        let id = data.length - 1;
+        console.log(data[id])
+        self.name = data[id].name;
+        self.description = data[id].summary;
+        self.id = data[id].id;
+        self.getCoverGame(data[id].cover,function(data){
           self.imageUrl = "https://" + data[0].url.replace("//","")
           self.load = true
         });
-        for(let i = 0; i < data[0].genres.length; i++)
-          self.getGenre(data[0].genres[i],function(data){
+        for(let i = 0; i < data[id].genres.length; i++)
+          self.getGenre(data[id].genres[i],function(data){
             self.genres.push(data[0].name)
           });
-        for(let i = 0; i < data[0].themes.length; i++)
-          self.getTheme(data[0].themes[i],function(data){
+        for(let i = 0; i < data[id].themes.length; i++)
+          self.getTheme(data[id].themes[i],function(data){
             self.theme.push(data[0].name)
           });
       });
